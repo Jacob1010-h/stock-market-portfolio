ea42786e37a571da8291ec98e62eda11428b5209
Jacob1010-h <lorgaming1010@gmail.com>
2024-05-19 18:53:37 +0000

    complete step 2

--- src/App.js
+++ src/App.js
@@ -12,8 +12,19 @@ function App() {
   const [stocks, setStocks] = useState([]);
   const [stockList, setStockList] = useState([]);
   
+  const [stockPrices, setStockPrices] = useState({});
+  const [tickerList, setTickerList] = useState([]);
+  const [portfolioData, setPortfolioData] = useState([]);
+  
   const AWS_API_GATEWAY = "https://n1haz2qiqi.execute-api.us-east-1.amazonaws.com/prod";
   const AWS_API_GATEWAY_GET_PORTFOLIO = AWS_API_GATEWAY + '/get-portfolio';
+  const AWS_API_GATEWAY_GET_STOCK_PRICE = AWS_API_GATEWAY + '/get-stock-price';
+  
+  
+  useEffect(() => {
+    const tickers = createTickerList(stocks);
+    setTickerList(tickers);
+  }, [stocks])
   
   // Retrieve the current stock information when the page first loads
   useEffect(() => {
@@ -29,32 +40,7 @@ function App() {
         }
         return response.json();
       })
-      .then(function(response) {
-        const stocks = response.Items;
-        let betterStocks = [];
-        
-        // {
-        //   "ticker":"",
-        //   "shares":0,
-        //   "purchasePrice":0.0,
-        //   "name":""
-        // }
-        for(let i = 0; i < stocks.length; i++) {
-          const ticker = Object.values(stocks[i].ticker);
-          const shares = Object.values(stocks[i].shares);
-          const purchasePrice = Object.values(stocks[i].purchasePrice);
-          const name = Object.values(stocks[i].name);
-          
-          betterStocks[i] = {
-            "ticker" : ticker[0],
-            "shares" : shares[0],
-            "purchasePrice" : purchasePrice[0],
-            "name" : name[0]
-          }
-        }
-          console.log(betterStocks);
-          setStocks(betterStocks)
-      })
+      .then((response) => getPortfolio(response) )
       .catch(function(error) {
         console.log(error);
       })
@@ -74,6 +60,61 @@ function App() {
     })
     setStockList(enhancedStocks);
   }, [stocks])
+  
+  
+  const createTickerList = (portfolio) => {
+    return portfolio.reduce((tickers, item) => {
+      tickers.push(item.ticker);
+      return tickers;
+    }, []);
+  }
+  
+  const getPortfolio = (response) => {
+    const stocks = response.Items;
+    let betterStocks = [];
+    
+    // {
+    //   "ticker":"",
+    //   "shares":0,
+    //   "purchasePrice":0.0,
+    //   "name":"" // not needed
+    // }
+    for(let i = 0; i < stocks.length; i++) {
+      const ticker = Object.values(stocks[i].ticker);
+      const shares = Object.values(stocks[i].shares);
+      const purchasePrice = Object.values(stocks[i].purchasePrice);
+      
+      betterStocks[i] = {
+        "ticker" : ticker[0],
+        "shares" : shares[0],
+        "purchasePrice" : purchasePrice[0]
+      }
+    }
+    // console.log(betterStocks);
+    console.log(createTickerList(betterStocks));
+    setStocks(betterStocks)
+  }
+  
+  
+  const getStockPrice = (ticker) => {
+    return new Promise((resolve, reject) => {
+      const fetchOptions = {
+        methos: 'POST', 
+        cache: 'default',
+        body: JSON.stringify({ticker:ticker})
+      }
+      
+      fetch(AWS_API_GATEWAY_GET_STOCK_PRICE, fetchOptions)
+      .then(response => {
+        if(!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => resolve(data))
+      .catch(error => reject(error));
+    });
+  }
   
   const addStock = evt => {
     console.log('add stock clicked');
